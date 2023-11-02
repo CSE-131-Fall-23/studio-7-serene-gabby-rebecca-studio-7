741be642bd1e04a947d00236909d74f5464a6996
rebec <rebec@RTYLaptop.dhcp.wustl.edu>
2023-11-02 12:43:59 -0500

    yay

--- src/studio7/Fraction.java
+++ src/studio7/Fraction.java
@@ -1,6 +1,13 @@
 package studio7;
 
 public class Fraction {
+	private int numerator;
+	private int denominator;
+
+	public Fraction(int initNumerator, int initDenominator) {
+		this.numerator = initNumerator;
+		this.denominator = initDenominator;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
